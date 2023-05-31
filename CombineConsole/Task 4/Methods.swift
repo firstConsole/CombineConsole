e905783863fd1f1a1727ceaa09cbd9e75fd8c00a
Артамонов Алексей Викторович <94192633+firstConsole@users.noreply.github.com>
2023-05-31 20:59:51 +0300

    Task 4

---
+++ CombineConsole/Task 4/Methods.swift
@@ -0,0 +1,13 @@
+//
+//  Methods.swift
+//  CombineConsole
+//
+//  Created by Алексей Артамонов on 28.05.2023.
+//
+
+import Foundation
+
+enum URLConfig: String {
+    case scheme = "https"
+    case host = "api.weatherapi.com"
+}
